b51afdd98bfe1625dc7ef4ee075e3809265818db
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 19:24:33 +0600

    pass test, button rendered with config

--- src/components/start-button/StartButton.test.tsx
+++ src/components/start-button/StartButton.test.tsx
@@ -1,11 +1,21 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { StartButton } from './StartButton';
+import { TestElement } from '../../enums/TestElement';
+import { gameConfig } from '../../config/game-config';
 
 afterEach(cleanup);
 
+let button: Element | null;
+
+beforeAll(() => {
+  const { container } = render(<StartButton />);
+  button = container.querySelector(`[data-testid="${TestElement.START_BUTTON}"]`);
+});
+
 describe('[GAME] StartButton', () => {
   it('SHOULD Render StartButton with proper config', () => {
-    //
+    expect(button).toHaveTextContent(gameConfig.startButton.buttonText);
   });
 
   it('SHOULD Render StartButton with click event', () => {
