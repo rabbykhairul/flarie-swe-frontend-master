75c0224f7ff37b1ea04bad50a2986358cd972d23
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 21:46:24 +0600

    make the game box responsive

--- src/components/game-box/styled.ts
+++ src/components/game-box/styled.ts
@@ -14,5 +14,15 @@ export const ContainerStyled = styled.div(
     backgroundColor: '#a5f3fc',
     borderRadius: gameConfig.container.borderRadius,
     boxShadow: '0 0 24px 8px #0284c7',
+
+    '@media(max-width: 800px)': {
+      width: '337.5px',
+      height: '600px',
+    },
+
+    '@media(max-width: 320px)': {
+      width: '320px',
+      height: '569px',
+    },
   }),
 );
