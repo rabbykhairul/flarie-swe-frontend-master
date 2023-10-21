6b31dc862a08437a24c68fc58206c8a231336b59
rabbykhairul <rabbykhairul@gmail.com>
2023-10-21 21:26:10 +0600

    test campaign banner style

--- src/components/campaign-banner/CampaignBanner.test.tsx
+++ src/components/campaign-banner/CampaignBanner.test.tsx
@@ -1,11 +1,34 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { App } from '../../app/App';
+import { TestElement } from '../../enums/TestElement';
+import { gameConfig } from '../../config/game-config';
 
 afterEach(cleanup);
 
+let banner: Element | null;
+
+beforeEach(() => {
+  const { container } = render(<App />);
+  banner = container.querySelector(`[data-testid="${TestElement.CAMPAIGN_BANNER}"]`);
+});
+
 describe('[GAME] CampaignBanner', () => {
   it('SHOULD Render CampaignBanner with all the CSS properties from config file', () => {
-    //
+    const style = getComputedStyle(banner as Element);
+
+    expect(banner).toHaveStyle({
+      display: 'flex',
+      position: 'absolute',
+      flexDirection: 'column',
+      textAlign: 'center',
+      top: gameConfig.campaignBanner.offsetTop,
+      borderRadius: `${gameConfig.campaignBanner.borderRadius}px`,
+      width: gameConfig.campaignBanner.width,
+      padding: `${gameConfig.campaignBanner.padding}px`,
+      background: gameConfig.campaignBanner.background,
+      color: gameConfig.campaignBanner.textColor,
+    });
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN viewport width is 250 px', () => {
