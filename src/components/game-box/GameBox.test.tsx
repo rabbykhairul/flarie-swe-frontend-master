6db8d45e3713da67ecbac456dd4ce2a3ac6c6fe1
rabbykhairul <rabbykhairul@gmail.com>
2023-10-22 00:41:37 +0600

    game box, pass 3rd test

--- src/components/game-box/GameBox.test.tsx
+++ src/components/game-box/GameBox.test.tsx
@@ -45,6 +45,26 @@ describe('[GAME] GameBox', () => {
   });
 
   it('SHOULD Render GameBox with height 600px WHEN browsers viewport width 1000px and height is 600px', () => {
-    //
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 1000,
+    });
+
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: 600,
+    });
+
+    fireEvent.resize(window);
+
+    let gameBox: Element | null;
+    const { container } = render(<App />);
+    gameBox = container.querySelector(`[data-testid="${TestElement.GAME_BOX}"]`);
+
+    expect(gameBox).toHaveStyle({
+      height: `600px`,
+    });
   });
 });
